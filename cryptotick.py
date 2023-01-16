fc6d205dc3f60d7df48d21776d9e4929ec0ca791
llvlch <mart@llvll.ch>
2023-01-16 21:37:01 +0100

    tyop

--- cryptotick.py
+++ cryptotick.py
@@ -54,17 +54,21 @@ def stoic(img, config):
 
             sourcestring=rawquote['author']
             quotestring=rawquote['text']
-
-            img, numline =writewrappedlines(img,quote,fontsize,y_text,height, width,fontstring)
+            fontstring = "JosefinSans-Light"
+            y_text= -300
+            height= 110
+            width= 38
+            fontsize=70
+            img, numline =writewrappedlines(img,quotestring,fontsize,y_text,height, width,fontstring)
             draw.line((500,880, 948,880), fill=255, width=3)
-            _place_text(img,source,0,430,70,"JosefinSans-Light")
+            _place_text(img,sourcestring,0,430,70,"JosefinSans-Light")
             if numline<7 and numline >0:
                 success=True
                 break
             else:
                 img = Image.new("RGB", (1448, 1072), color = (255, 255, 255) )
     except Exception as e:
-        message="Interlude doe to a data pull/print problem (Daily Stoic)"
+        message="Interlude due to a data pull/print problem (Daily Stoic)"
         img = beanaproblem(img, message)
         success=False
         time.sleep(10)
